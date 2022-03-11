99db1a3a59ab3a3684dae438ac256016ae025db7
Simon <simon@benezan.de>
2022-03-11 09:28:51 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,14 @@ module.exports = {
 
   deploy : {
     production : {
+      user : 'root',
+      host : '81.169.159.21',
       ref  : 'origin/master',
-      repo : 'git@github.com:Frooastside/Brotisch.git',
+      repo : 'git@github.com/Frooastside/Brotisch.git',
       path : '/root/Brotisch',
+      'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
     }
   }
 };
